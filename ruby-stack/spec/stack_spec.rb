37606ddb19aef91205f55cae69f125228d9c4b8b
richard-to <gohn76@gmail.com>
2015-03-30 10:49:15 -0700

    Add test for empty method

--- ruby-stack/spec/stack_spec.rb
+++ ruby-stack/spec/stack_spec.rb
@@ -4,4 +4,9 @@ describe Stack do
   it "can be created" do
     stack = Stack.new
   end
+
+  it "can check if it is empty" do
+    stack = Stack.new
+    expect(stack.empty?).to be_truthy
+  end
 end
